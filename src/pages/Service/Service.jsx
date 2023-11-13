9d0fc664186c78c47af0080c3bf228763efd3db2
Albert Tazetdinov <zet565758@gmail.com>
2023-11-13 15:36:09 +0300

    test

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -36,6 +36,7 @@ export const Service = () => {
                             <ServiceItem
                                 key={service.id}
                                 service={service}
+
                             />
                         )
                     })
